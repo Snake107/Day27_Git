70f3c656ce64da017f3dd51db9c2f9b864e6cb4a
Snake107 <501159870@qq.com>
2019-06-16 14:04:33 +0800

    Initial commit

---
+++ src/com/snake/git/Demo.java
@@ -0,0 +1,12 @@
+package com.snake.git;
+
+/**
+ * @version 1.0.0
+ * @auther Snake
+ * @date 2019/6/16 13:55
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
